8a0d9425a9d8f331d86fbeb8b53c5566f113f6fa
Bartek Brzyski <the.bainow03@gmail.com>
2022-10-07 17:14:51 +0200

    add basic structre of mvector

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
+template <typename T>
+class Mvector{
+private:
+    int current_size;
+    int number_of_elements;
+    T* main_buffer;
+    
+public:
+    Mvector(){
+        current_size = 10;
+        main_buffer = new T[current_size];
+        number_of_elements = 0;
+    }
+
+    ~Mvector(){
+        delete main_buffer;
+    }
+};
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Mvector<int> test;
     return 0;
 }
-    
